6638100bb00590d9854354b948c50875639b544d
Aki Rodic <aleksandar.xyz@gmail.com>
2019-02-27 00:32:57 +0100

    patch

--- src/helpers/Selection.js
+++ src/helpers/Selection.js
@@ -57,62 +57,62 @@ export class SelectionHelper extends Helper {
     // super.add(axis);
   }
   selectionChanged() {
-    const selected = this.selection.selected;
-
-    if (selected.length && selected[0].geometry) {
-      const object = selected[0];
-
-      if (!object.geometry.boundingBox) object.geometry.computeBoundingBox();
-      const bbMax = object.geometry.boundingBox.max;
-      const bbMin = object.geometry.boundingBox.min;
-      // bbMax.applyMatrix4(object.matrixWorld);
-      // bbMin.applyMatrix4(object.matrixWorld);
-
-      this.corners['XYZ'].position.set(bbMax.x, bbMax.y, bbMax.z);
-      this.corners['XYz'].position.set(bbMax.x, bbMax.y, bbMin.z);
-      this.corners['xyz'].position.set(bbMin.x, bbMin.y, bbMin.z);
-      this.corners['xyZ'].position.set(bbMin.x, bbMin.y, bbMax.z);
-      this.corners['xYZ'].position.set(bbMin.x, bbMax.y, bbMax.z);
-      this.corners['xYz'].position.set(bbMin.x, bbMax.y, bbMin.z);
-      this.corners['Xyz'].position.set(bbMax.x, bbMin.y, bbMin.z);
-      this.corners['XyZ'].position.set(bbMax.x, bbMin.y, bbMax.z);
-
-      //
-
-      object.updateMatrixWorld();
-      object.matrixWorld.decompose(_position, _quaternion, _scale);
-
-      _m1.compose(this.position, this.quaternion, _one);
-
-      _scale.x = Math.abs(_scale.x);
-      _scale.y = Math.abs(_scale.y);
-      _scale.z = Math.abs(_scale.z);
-
-      for (let i = 0; i < 8; i ++) {
-
-        _position.copy(this.children[i].position).multiply(_scale);
-
-        let __scale = this.scale.clone();
-
-        let dir = this.children[i].position.clone().applyQuaternion(this.quaternion).normalize();
-
-        this.children[i].material.highlight = Math.min(Math.max(3 - Math.abs(dir.dot(this.eye)) * 4, -1), 0.5);
-
-        __scale.x = Math.min(this.scale.x, Math.abs(_position.x) / 2);
-        __scale.y = Math.min(this.scale.y, Math.abs(_position.y) / 2);
-        __scale.z = Math.min(this.scale.z, Math.abs(_position.z) / 2);
-
-        __scale.x = Math.max(__scale.x, EPS);
-        __scale.y = Math.max(__scale.y, EPS);
-        __scale.z = Math.max(__scale.z, EPS);
-
-        _m2.compose(_position, new Quaternion, __scale);
-
-        this.children[i].matrixWorld.copy(_m1).multiply(_m2);
-      }
-
-
-    }
+    // const selected = this.selection.selected;
+    //
+    // if (selected.length && selected[0].geometry) {
+    //   const object = selected[0];
+    //
+    //   if (!object.geometry.boundingBox) object.geometry.computeBoundingBox();
+    //   const bbMax = object.geometry.boundingBox.max;
+    //   const bbMin = object.geometry.boundingBox.min;
+    //   // bbMax.applyMatrix4(object.matrixWorld);
+    //   // bbMin.applyMatrix4(object.matrixWorld);
+    //
+    //   this.corners['XYZ'].position.set(bbMax.x, bbMax.y, bbMax.z);
+    //   this.corners['XYz'].position.set(bbMax.x, bbMax.y, bbMin.z);
+    //   this.corners['xyz'].position.set(bbMin.x, bbMin.y, bbMin.z);
+    //   this.corners['xyZ'].position.set(bbMin.x, bbMin.y, bbMax.z);
+    //   this.corners['xYZ'].position.set(bbMin.x, bbMax.y, bbMax.z);
+    //   this.corners['xYz'].position.set(bbMin.x, bbMax.y, bbMin.z);
+    //   this.corners['Xyz'].position.set(bbMax.x, bbMin.y, bbMin.z);
+    //   this.corners['XyZ'].position.set(bbMax.x, bbMin.y, bbMax.z);
+    //
+    //   //
+    //
+    //   object.updateMatrixWorld();
+    //   object.matrixWorld.decompose(_position, _quaternion, _scale);
+    //
+    //   _m1.compose(this.position, this.quaternion, _one);
+    //
+    //   _scale.x = Math.abs(_scale.x);
+    //   _scale.y = Math.abs(_scale.y);
+    //   _scale.z = Math.abs(_scale.z);
+    //
+    //   for (let i = 0; i < 8; i ++) {
+    //
+    //     _position.copy(this.children[i].position).multiply(_scale);
+    //
+    //     let __scale = this.scale.clone();
+    //
+    //     let dir = this.children[i].position.clone().applyQuaternion(this.quaternion).normalize();
+    //
+    //     this.children[i].material.highlight = Math.min(Math.max(3 - Math.abs(dir.dot(this.eye)) * 4, -1), 0.5);
+    //
+    //     __scale.x = Math.min(this.scale.x, Math.abs(_position.x) / 2);
+    //     __scale.y = Math.min(this.scale.y, Math.abs(_position.y) / 2);
+    //     __scale.z = Math.min(this.scale.z, Math.abs(_position.z) / 2);
+    //
+    //     __scale.x = Math.max(__scale.x, EPS);
+    //     __scale.y = Math.max(__scale.y, EPS);
+    //     __scale.z = Math.max(__scale.z, EPS);
+    //
+    //     _m2.compose(_position, new Quaternion, __scale);
+    //
+    //     this.children[i].matrixWorld.copy(_m1).multiply(_m2);
+    //   }
+    //
+    //
+    // }
   }
 
   updateMatrixWorld() {
