3f72768892d531549d940154183e488ea6fe257f
Aki Rodic <aleksandar.xyz@gmail.com>
2019-09-12 21:35:48 +0100

    Added texture options.

--- src/inspector/inspector.js
+++ src/inspector/inspector.js
@@ -7,7 +7,7 @@ function makeOptions(list) {
 	for (let i = 0; i < list.length; i++) {
 		options.push({value: THREE[list[i]], label: list[i]});
 	}
-	return ['io-option-menu', {'options': options}];
+	return options;
 }
 
 const floatSlider = ['io-number-slider', {min: 0, max: 1, step: 0.001}];
@@ -20,6 +20,10 @@ const matrixProp = ['io-properties', {
 
 const propProp = ['io-properties'];
 
+const textureFormatOptions = makeOptions(['AlphaFormat', 'RGBFormat', 'RGBAFormat', 'LuminanceFormat', 'LuminanceAlphaFormat', 'RGBEFormat', 'DepthFormat', 'DepthStencilFormat', 'RedFormat', 'RGB_ETC1_Format']);
+textureFormatOptions.push({label: 'DXT Formats', options: makeOptions(['RGB_S3TC_DXT1_Format', 'RGBA_S3TC_DXT1_Format', 'RGBA_S3TC_DXT3_Format', 'RGBA_S3TC_DXT5_Format'])});
+textureFormatOptions.push({label: 'PVRTC Formats', options: makeOptions(['RGB_PVRTC_4BPPV1_Format', 'RGB_PVRTC_2BPPV1_Format', 'RGBA_PVRTC_4BPPV1_Format', 'RGBA_PVRTC_2BPPV1_Format'])});
+textureFormatOptions.push({label: 'ASTC Formats', options: makeOptions(['RGBA_ASTC_4x4_Format', 'RGBA_ASTC_5x4_Format', 'RGBA_ASTC_5x5_Format', 'RGBA_ASTC_6x5_Format', 'RGBA_ASTC_6x6_Format', 'RGBA_ASTC_8x5_Format', 'RGBA_ASTC_8x6_Format', 'RGBA_ASTC_8x8_Format', 'RGBA_ASTC_10x5_Format', 'RGBA_ASTC_10x6_Format', 'RGBA_ASTC_10x8_Format', 'RGBA_ASTC_10x10_Format', 'RGBA_ASTC_12x10_Format', 'RGBA_ASTC_12x12_Format'])});
 const propConfig = {
 	// Basic types
 	'type:boolean': ['io-switch'],
@@ -36,7 +40,7 @@ const propConfig = {
 	// Other types
 	'constructor:Sphere': propProp, // Temp
 	// Object3D
-	'Object3D|drawMode': makeOptions(['TrianglesDrawMode', 'TriangleStripDrawMode', 'TriangleFanDrawMode']),
+	'Object3D|drawMode': ['io-option-menu', {'options': makeOptions(['TrianglesDrawMode', 'TriangleStripDrawMode', 'TriangleFanDrawMode'])}],
 	// BufferGeometry
 	'BufferGeometry|constructor:Object': propProp,
 	// 'BufferGeometry|index': ['three-attributes'],
@@ -48,41 +52,41 @@ const propConfig = {
 	// 'Material|aoMapIntensity': ['io-slider', {'min': 0,'max': 1}],
 	// 'Material|lightMapIntensity': ['io-slider', {'min': 0,'max': 1}],
 	'Material|opacity': floatSlider,
-	'Material|side': makeOptions(['FrontSide', 'BackSide', 'DoubleSide']),
-	'Material|shading': makeOptions(['FlatShading', 'SmoothShading']),
-	'Material|vertexColors': makeOptions(['NoColors', 'FaceColors', 'VertexColors']),
-	'Material|blending': makeOptions(['NoBlending', 'NormalBlending', 'AdditiveBlending', 'SubtractiveBlending', 'MultiplyBlending', 'CustomBlending']),
-	'Material|blendEquation': makeOptions(['AddEquation', 'SubtractEquation', 'ReverseSubtractEquation', 'MinEquation', 'MaxEquation']),
-	'Material|blendEquationAlpha': makeOptions(['AddEquation', 'SubtractEquation', 'ReverseSubtractEquation', 'MinEquation', 'MaxEquation']),
-	'Material|blendSrc': makeOptions(['ZeroFactor', 'OneFactor', 'SrcColorFactor', 'OneMinusSrcColorFactor', 'SrcAlphaFactor', 'OneMinusSrcAlphaFactor', 'DstAlphaFactor', 'OneMinusDstAlphaFactor', 'DstColorFactor', 'OneMinusDstColorFactor', 'SrcAlphaSaturateFactor']),
-	'Material|blendDst': makeOptions(['ZeroFactor', 'OneFactor', 'SrcColorFactor', 'OneMinusSrcColorFactor', 'SrcAlphaFactor', 'OneMinusSrcAlphaFactor', 'DstAlphaFactor', 'OneMinusDstAlphaFactor', 'DstColorFactor', 'OneMinusDstColorFactor', 'SrcAlphaSaturateFactor']),
-	'Material|blendSrcAlpha': makeOptions(['ZeroFactor', 'OneFactor', 'SrcColorFactor', 'OneMinusSrcColorFactor', 'SrcAlphaFactor', 'OneMinusSrcAlphaFactor', 'DstAlphaFactor', 'OneMinusDstAlphaFactor', 'DstColorFactor', 'OneMinusDstColorFactor', 'SrcAlphaSaturateFactor']),
-	'Material|blendDstAlpha': makeOptions(['ZeroFactor', 'OneFactor', 'SrcColorFactor', 'OneMinusSrcColorFactor', 'SrcAlphaFactor', 'OneMinusSrcAlphaFactor', 'DstAlphaFactor', 'OneMinusDstAlphaFactor', 'DstColorFactor', 'OneMinusDstColorFactor', 'SrcAlphaSaturateFactor']),
-	'Material|depthFunc': makeOptions(['NeverDepth', 'AlwaysDepth', 'LessDepth', 'LessEqualDepth', 'EqualDepth', 'GreaterEqualDepth', 'GreaterDepth', 'NotEqualDepth']),
-	'Material|combine': makeOptions(['MultiplyOperation', 'MixOperation', 'AddOperation']),
-	'Material|shadowSide': makeOptions(['BackSide', 'FrontSide', 'DoubleSide']),
-	'Material|stencilFunc': makeOptions(['NeverStencilFunc', 'LessStencilFunc', 'EqualStencilFunc', 'LessEqualStencilFunc', 'GreaterStencilFunc', 'NotEqualStencilFunc', 'GreaterEqualStencilFunc', 'AlwaysStencilFunc']),
-	'Material|stencilFail': makeOptions(['ZeroStencilOp', 'KeepStencilOp', 'ReplaceStencilOp', 'IncrementStencilOp', 'DecrementStencilOp', 'IncrementWrapStencilOp', 'DecrementWrapStencilOp', 'InvertStencilOp']),
-	'Material|stencilZFail': makeOptions(['ZeroStencilOp', 'KeepStencilOp', 'ReplaceStencilOp', 'IncrementStencilOp', 'DecrementStencilOp', 'IncrementWrapStencilOp', 'DecrementWrapStencilOp', 'InvertStencilOp']),
-	'Material|stencilZPass': makeOptions(['ZeroStencilOp', 'KeepStencilOp', 'ReplaceStencilOp', 'IncrementStencilOp', 'DecrementStencilOp', 'IncrementWrapStencilOp', 'DecrementWrapStencilOp', 'InvertStencilOp']),
-	'Material|depthPacking': makeOptions(['BasicDepthPacking', 'RGBADepthPacking']),
-	'Material|normalMapType': makeOptions(['TangentSpaceNormalMap', 'ObjectSpaceNormalMap']),
+	'Material|side': ['io-option-menu', {'options': makeOptions(['FrontSide', 'BackSide', 'DoubleSide'])}],
+	'Material|shading': ['io-option-menu', {'options': makeOptions(['FlatShading', 'SmoothShading'])}],
+	'Material|vertexColors': ['io-option-menu', {'options': makeOptions(['NoColors', 'FaceColors', 'VertexColors'])}],
+	'Material|blending': ['io-option-menu', {'options': makeOptions(['NoBlending', 'NormalBlending', 'AdditiveBlending', 'SubtractiveBlending', 'MultiplyBlending', 'CustomBlending'])}],
+	'Material|blendEquation': ['io-option-menu', {'options': makeOptions(['AddEquation', 'SubtractEquation', 'ReverseSubtractEquation', 'MinEquation', 'MaxEquation'])}],
+	'Material|blendEquationAlpha': ['io-option-menu', {'options': makeOptions(['AddEquation', 'SubtractEquation', 'ReverseSubtractEquation', 'MinEquation', 'MaxEquation'])}],
+	'Material|blendSrc': ['io-option-menu', {'options': makeOptions(['ZeroFactor', 'OneFactor', 'SrcColorFactor', 'OneMinusSrcColorFactor', 'SrcAlphaFactor', 'OneMinusSrcAlphaFactor', 'DstAlphaFactor', 'OneMinusDstAlphaFactor', 'DstColorFactor', 'OneMinusDstColorFactor', 'SrcAlphaSaturateFactor'])}],
+	'Material|blendDst': ['io-option-menu', {'options': makeOptions(['ZeroFactor', 'OneFactor', 'SrcColorFactor', 'OneMinusSrcColorFactor', 'SrcAlphaFactor', 'OneMinusSrcAlphaFactor', 'DstAlphaFactor', 'OneMinusDstAlphaFactor', 'DstColorFactor', 'OneMinusDstColorFactor', 'SrcAlphaSaturateFactor'])}],
+	'Material|blendSrcAlpha': ['io-option-menu', {'options': makeOptions(['ZeroFactor', 'OneFactor', 'SrcColorFactor', 'OneMinusSrcColorFactor', 'SrcAlphaFactor', 'OneMinusSrcAlphaFactor', 'DstAlphaFactor', 'OneMinusDstAlphaFactor', 'DstColorFactor', 'OneMinusDstColorFactor', 'SrcAlphaSaturateFactor'])}],
+	'Material|blendDstAlpha': ['io-option-menu', {'options': makeOptions(['ZeroFactor', 'OneFactor', 'SrcColorFactor', 'OneMinusSrcColorFactor', 'SrcAlphaFactor', 'OneMinusSrcAlphaFactor', 'DstAlphaFactor', 'OneMinusDstAlphaFactor', 'DstColorFactor', 'OneMinusDstColorFactor', 'SrcAlphaSaturateFactor'])}],
+	'Material|depthFunc': ['io-option-menu', {'options': makeOptions(['NeverDepth', 'AlwaysDepth', 'LessDepth', 'LessEqualDepth', 'EqualDepth', 'GreaterEqualDepth', 'GreaterDepth', 'NotEqualDepth'])}],
+	'Material|combine': ['io-option-menu', {'options': makeOptions(['MultiplyOperation', 'MixOperation', 'AddOperation'])}],
+	'Material|shadowSide': ['io-option-menu', {'options': makeOptions(['BackSide', 'FrontSide', 'DoubleSide'])}],
+	'Material|stencilFunc': ['io-option-menu', {'options': makeOptions(['NeverStencilFunc', 'LessStencilFunc', 'EqualStencilFunc', 'LessEqualStencilFunc', 'GreaterStencilFunc', 'NotEqualStencilFunc', 'GreaterEqualStencilFunc', 'AlwaysStencilFunc'])}],
+	'Material|stencilFail': ['io-option-menu', {'options': makeOptions(['ZeroStencilOp', 'KeepStencilOp', 'ReplaceStencilOp', 'IncrementStencilOp', 'DecrementStencilOp', 'IncrementWrapStencilOp', 'DecrementWrapStencilOp', 'InvertStencilOp'])}],
+	'Material|stencilZFail': ['io-option-menu', {'options': makeOptions(['ZeroStencilOp', 'KeepStencilOp', 'ReplaceStencilOp', 'IncrementStencilOp', 'DecrementStencilOp', 'IncrementWrapStencilOp', 'DecrementWrapStencilOp', 'InvertStencilOp'])}],
+	'Material|stencilZPass': ['io-option-menu', {'options': makeOptions(['ZeroStencilOp', 'KeepStencilOp', 'ReplaceStencilOp', 'IncrementStencilOp', 'DecrementStencilOp', 'IncrementWrapStencilOp', 'DecrementWrapStencilOp', 'InvertStencilOp'])}],
+	'Material|depthPacking': ['io-option-menu', {'options': makeOptions(['BasicDepthPacking', 'RGBADepthPacking'])}],
+	'Material|normalMapType': ['io-option-menu', {'options': makeOptions(['TangentSpaceNormalMap', 'ObjectSpaceNormalMap'])}],
 	// Texture
-	'Texture|mapping': makeOptions(['UVMapping', 'CubeReflectionMapping', 'CubeRefractionMapping', 'EquirectangularReflectionMapping', 'EquirectangularRefractionMapping', 'SphericalReflectionMapping', 'CubeUVReflectionMapping', 'CubeUVRefractionMapping']),
-	'Texture|wrapS': makeOptions(['RepeatWrapping', 'ClampToEdgeWrapping', 'MirroredRepeatWrapping']),
-	'Texture|wrapT': makeOptions(['RepeatWrapping', 'ClampToEdgeWrapping', 'MirroredRepeatWrapping']),
-	'Texture|minFilter': makeOptions(['NearestFilter', 'NearestMipMapNearestFilter', 'NearestMipMapLinearFilter', 'LinearFilter', 'LinearMipMapNearestFilter', 'LinearMipMapLinearFilter']),
-	'Texture|magFilter': makeOptions(['NearestFilter', 'NearestMipMapNearestFilter', 'NearestMipMapLinearFilter', 'LinearFilter', 'LinearMipMapNearestFilter', 'LinearMipMapLinearFilter']),
-	'Texture|type': makeOptions(['UnsignedByteType', 'ByteType', 'ShortType', 'UnsignedShortType', 'IntType', 'UnsignedIntType', 'FloatType', 'HalfFloatType', 'UnsignedShort4444Type', 'UnsignedShort5551Type', 'UnsignedShort565Type', 'UnsignedInt248Type']),
-	'Texture|encoding': makeOptions(['LinearEncoding', 'sRGBEncoding', 'GammaEncoding', 'RGBEEncoding', 'LogLuvEncoding', 'RGBM7Encoding', 'RGBM16Encoding', 'RGBDEncoding']),
-	'Texture|format': makeOptions(['AlphaFormat', 'RGBFormat', 'RGBAFormat', 'LuminanceFormat', 'LuminanceAlphaFormat', 'RGBEFormat', 'DepthFormat', 'DepthStencilFormat', 'RedFormat', 'RGB_S3TC_DXT1_Format', 'RGBA_S3TC_DXT1_Format', 'RGBA_S3TC_DXT3_Format', 'RGBA_S3TC_DXT5_Format', 'RGB_PVRTC_4BPPV1_Format', 'RGB_PVRTC_2BPPV1_Format', 'RGBA_PVRTC_4BPPV1_Format', 'RGBA_PVRTC_2BPPV1_Format', 'RGB_ETC1_Format', 'RGBA_ASTC_4x4_Format', 'RGBA_ASTC_5x4_Format', 'RGBA_ASTC_5x5_Format', 'RGBA_ASTC_6x5_Format', 'RGBA_ASTC_6x6_Format', 'RGBA_ASTC_8x5_Format', 'RGBA_ASTC_8x6_Format', 'RGBA_ASTC_8x8_Format', 'RGBA_ASTC_10x5_Format', 'RGBA_ASTC_10x6_Format', 'RGBA_ASTC_10x8_Format', 'RGBA_ASTC_10x10_Format', 'RGBA_ASTC_12x10_Format', 'RGBA_ASTC_12x12_Format']),
-	'Texture|unpackAlignment': makeOptions(['1', '2', '4', '8']),
+	'Texture|mapping': ['io-option-menu', {'options': makeOptions(['UVMapping', 'CubeReflectionMapping', 'CubeRefractionMapping', 'EquirectangularReflectionMapping', 'EquirectangularRefractionMapping', 'SphericalReflectionMapping', 'CubeUVReflectionMapping', 'CubeUVRefractionMapping'])}],
+	'Texture|wrapS': ['io-option-menu', {'options': makeOptions(['RepeatWrapping', 'ClampToEdgeWrapping', 'MirroredRepeatWrapping'])}],
+	'Texture|wrapT': ['io-option-menu', {'options': makeOptions(['RepeatWrapping', 'ClampToEdgeWrapping', 'MirroredRepeatWrapping'])}],
+	'Texture|minFilter': ['io-option-menu', {'options': makeOptions(['NearestFilter', 'NearestMipMapNearestFilter', 'NearestMipMapLinearFilter', 'LinearFilter', 'LinearMipMapNearestFilter', 'LinearMipMapLinearFilter'])}],
+	'Texture|magFilter': ['io-option-menu', {'options': makeOptions(['NearestFilter', 'NearestMipMapNearestFilter', 'NearestMipMapLinearFilter', 'LinearFilter', 'LinearMipMapNearestFilter', 'LinearMipMapLinearFilter'])}],
+	'Texture|type': ['io-option-menu', {'options': makeOptions(['UnsignedByteType', 'ByteType', 'ShortType', 'UnsignedShortType', 'IntType', 'UnsignedIntType', 'FloatType', 'HalfFloatType', 'UnsignedShort4444Type', 'UnsignedShort5551Type', 'UnsignedShort565Type', 'UnsignedInt248Type'])}],
+	'Texture|encoding': ['io-option-menu', {'options': makeOptions(['LinearEncoding', 'sRGBEncoding', 'GammaEncoding', 'RGBEEncoding', 'LogLuvEncoding', 'RGBM7Encoding', 'RGBM16Encoding', 'RGBDEncoding'])}],
+	'Texture|format': ['io-option-menu', {'options': textureFormatOptions}],
+	'Texture|unpackAlignment': ['io-option-menu', {'options': makeOptions(['1', '2', '4', '8'])}],
 	// TODO // FrontFaceDirectionCW, FrontFaceDirectionCCW, CullFaceNone, CullFaceBack, CullFaceFront, CullFaceFrontBack
-	'WebGLRenderer|toneMapping': makeOptions(['NoToneMapping', 'LinearToneMapping', 'ReinhardToneMapping', 'Uncharted2ToneMapping', 'CineonToneMapping', 'ACESFilmicToneMapping']),
-	'WebGLShadowMap|type': makeOptions(['BasicShadowMap', 'PCFShadowMap', 'PCFSoftShadowMap', 'VSMShadowMap']),
+	'WebGLRenderer|toneMapping': ['io-option-menu', {'options': makeOptions(['NoToneMapping', 'LinearToneMapping', 'ReinhardToneMapping', 'Uncharted2ToneMapping', 'CineonToneMapping', 'ACESFilmicToneMapping'])}],
+	'WebGLShadowMap|type': ['io-option-menu', {'options': makeOptions(['BasicShadowMap', 'PCFShadowMap', 'PCFSoftShadowMap', 'VSMShadowMap'])}],
 	// TODO // ZeroCurvatureEnding, ZeroSlopeEnding, WrapAroundEnding
-	'AnimationAction|loop': makeOptions(['LoopOnce', 'LoopRepeat', 'LoopPingPong']),
-	'KeyframeTrack|loop': makeOptions(['InterpolateDiscrete', 'InterpolateLinear', 'InterpolateSmooth']),
+	'AnimationAction|loop': ['io-option-menu', {'options': makeOptions(['LoopOnce', 'LoopRepeat', 'LoopPingPong'])}],
+	'KeyframeTrack|loop': ['io-option-menu', {'options': makeOptions(['InterpolateDiscrete', 'InterpolateLinear', 'InterpolateSmooth'])}],
 
 	// Camera
 	"Camera|fov": ["io-number-slider", {min: 0.001, max: 180, step: 1}],
@@ -95,6 +99,18 @@ export class IoThreeInspector extends IoInspector {
 	static get Properties() {
 		return {
 			autoExpand: ['properties', 'transform', 'rendering'],
+		};
+	}
+	static get Listeners() {
+		return {
+			'mousedown': 'stopPropagation',
+			'mouseup': 'stopPropagation',
+			'mousemove': 'stopPropagation',
+			'touchstart': 'stopPropagation',
+			'touchmove': 'stopPropagation',
+			'touchend': 'stopPropagation',
+			'keydown': 'stopPropagation',
+			'keyup': 'stopPropagation',
 		};
 	}
 	static get Config() {
@@ -126,6 +142,9 @@ export class IoThreeInspector extends IoInspector {
 			'Object|hidden': [/^is/, 'type', 'id', 'uuid'],
 			// TODO
 		};
+	}
+	stopPropagation(event) {
+		event.stopImmediatePropagation();
 	}
 	selectedMutated() {
 		this.dispatchEvent('change');
